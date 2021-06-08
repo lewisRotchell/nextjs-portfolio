e13932f96eeb213ccdd7ece642e21111043091de
Lewis Rotchell <rotchell92@gmail.com>
2021-06-08 21:21:31 +0100

    added more a11y

--- components/UI/MobileMenu.js
+++ components/UI/MobileMenu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import classes from "./MobileMenu.module.css";
+import FocusTrap from "focus-trap-react";
 import {
   disableBodyScroll,
   enableBodyScroll,
@@ -19,52 +20,59 @@ const MobileMenu = ({ menuIsOpen, toggleMenu }) => {
   }, [menuIsOpen]);
 
   return (
-    <div
-      ref={menu}
-      className={`${classes.mobileMenu}`}
-      style={{
-        transform: `${menuIsOpen ? "translateX(0%)" : "translateX(-100%)"}`,
+    <FocusTrap
+      active={menuIsOpen}
+      focusTrapOptions={{
+        clickOutsideDeactivates: true,
       }}
     >
-      <ul className={classes.navList}>
-        <li className={classes.navItems}>
-          <a
-            tabIndex={!menuIsOpen && "-1"}
-            onClick={() => toggleMenu()}
-            href="#home"
-          >
-            Home
-          </a>
-        </li>
-        <li className={classes.navItems}>
-          <a
-            tabIndex={!menuIsOpen && "-1"}
-            onClick={() => toggleMenu()}
-            href="#about"
-          >
-            About
-          </a>
-        </li>
-        <li className={classes.navItems}>
-          <a
-            tabIndex={!menuIsOpen && "-1"}
-            onClick={() => toggleMenu()}
-            href="#projects"
-          >
-            Projects
-          </a>
-        </li>
-        <li className={classes.navItems}>
-          <a
-            tabIndex={!menuIsOpen && "-1"}
-            onClick={() => toggleMenu()}
-            href="#contact"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div
+        ref={menu}
+        className={`${classes.mobileMenu}`}
+        style={{
+          transform: `${menuIsOpen ? "translateX(0%)" : "translateX(-100%)"}`,
+        }}
+      >
+        <ul className={classes.navList}>
+          <li className={classes.navItems}>
+            <a
+              tabIndex={menuIsOpen ? undefined : 1}
+              onClick={() => toggleMenu()}
+              href="#home"
+            >
+              Home
+            </a>
+          </li>
+          <li className={classes.navItems}>
+            <a
+              tabIndex={menuIsOpen ? undefined : 1}
+              onClick={() => toggleMenu()}
+              href="#about"
+            >
+              About
+            </a>
+          </li>
+          <li className={classes.navItems}>
+            <a
+              tabIndex={menuIsOpen ? undefined : 1}
+              onClick={() => toggleMenu()}
+              href="#projects"
+            >
+              Projects
+            </a>
+          </li>
+          <li className={classes.navItems}>
+            <a
+              tabIndex={menuIsOpen ? undefined : 1}
+              onClick={() => toggleMenu()}
+              href="#contact"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </FocusTrap>
   );
 };
 
